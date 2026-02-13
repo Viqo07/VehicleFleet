1ce777cf87454e6607e857d3a6f490257b447455
Viqo07 <vicomus19@gmail.com>
2026-02-12 19:40:36 -0600

    Add files via upload

---
+++ Fleet.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System;
+
+namespace VehicleFleetManager
+{
+
+	using System.Collections.Generic;
+	using System.Linq;
+	using VehicleFleetManager;
+
+	public class Fleet
+	{
+		private List<Vehicle> _vehicles;
+
+		public Fleet()
+		{
+			_vehicles = new List<Vehicle>();
+		}
+
+		public void AddVehicle(Vehicle v)
+		{
+			_vehicles.Add(v);
+		}
+
+		public void RemoveVehicle(string model)
+		{
+			_vehicles.RemoveAll(v => v.Model.Equals(model, StringComparison.OrdinalIgnoreCase));
+		}
+
+		public double GetAverageMileage()
+		{
+			if (_vehicles.Count == 0)
+				return 0;
+
+			return _vehicles.Average(v => v.Mileage);
+		}
+
+		public void DisplayAllVehicles()
+		{
+			if (_vehicles.Count == 0)
+			{
+				Console.WriteLine("No vehicles in fleet.");
+				return;
+			}
+
+			foreach (var v in _vehicles)
+			{
+				Console.WriteLine(v.GetSummary());
+			}
+		}
+
+		public void ServiceAllDue()
+		{
+			int serviced = 0;
+
+			foreach (var v in _vehicles)
+			{
+				if (v.NeedsService())
+				{
+					v.PerformService();
+					serviced++;
+				}
+			}
+
+			Console.WriteLine($"{serviced} vehicles serviced.");
+		}
+	}
+}
