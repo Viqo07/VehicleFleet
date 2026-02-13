01318d96a91c2539e350dbbcc06488e3efb55ec6
Viqo07 <vicomus19@gmail.com>
2026-02-12 19:40:52 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System;
+
+namespace VehicleFleetManager
+{
+
+	using VehicleFleetManager;
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Fleet fleet = new Fleet();
+			bool running = true;
+
+			while (running)
+			{
+				Console.WriteLine("\n=== Vehicle Fleet Manager ===");
+				Console.WriteLine("1. Add Vehicle");
+				Console.WriteLine("2. Remove Vehicle");
+				Console.WriteLine("3. Display Fleet");
+				Console.WriteLine("4. Show Average Mileage");
+				Console.WriteLine("5. Service Due Vehicles");
+				Console.WriteLine("6. Exit");
+				Console.Write("Choose: ");
+
+				string choice = Console.ReadLine();
+
+				switch (choice)
+				{
+					case "1":
+						Console.Write("Enter Make: ");
+						string make = Console.ReadLine();
+
+						Console.Write("Enter Model: ");
+						string model = Console.ReadLine();
+
+						Console.Write("Enter Year: ");
+						int year = int.Parse(Console.ReadLine());
+
+						Console.Write("Enter Mileage: ");
+						double mileage = double.Parse(Console.ReadLine());
+
+						Vehicle v = new Vehicle(make, model, year, mileage);
+						fleet.AddVehicle(v);
+
+						Console.WriteLine("Vehicle added!");
+						break;
+
+					case "2":
+						Console.Write("Enter Model to remove: ");
+						string removeModel = Console.ReadLine();
+						fleet.RemoveVehicle(removeModel);
+						Console.WriteLine("Vehicle(s) removed.");
+						break;
+
+					case "3":
+						fleet.DisplayAllVehicles();
+						break;
+
+					case "4":
+						double avg = fleet.GetAverageMileage();
+						Console.WriteLine($"Average Mileage: {avg}");
+						break;
+
+					case "5":
+						fleet.ServiceAllDue();
+						break;
+
+					case "6":
+						running = false;
+						break;
+
+					default:
+						Console.WriteLine("Invalid choice.");
+						break;
+				}
+			}
+		}
+	}
+}
